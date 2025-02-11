9007f5c57c156ebe9a14270515decc28cad9140c
zephir-h <zephire.batut@ensta-paris.fr>
2025-02-11 09:34:44 +0100

    ajout de fonction d'adaptation

---
+++ src/fonction_adaption.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <utility>
+using namespace std;
+#include "genetique.cpp"
+
+double DistanceParcours(Individu I,vector<vector<double>> Matrice){
+    double dist = 0.;
+    int n = I.itineraire.size();
+    for(int i = 0; i < n-1;i++ ){
+        dist += Matrice[I.itineraire[i]][I.itineraire[i+1]];
+    }
+    return dist;
+}
