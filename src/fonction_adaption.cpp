7d7349f588a0144bfd09f0fad6e59eedf189df00
zephir-h <zephire.batut@ensta-paris.fr>
2025-02-11 09:49:32 +0100

    ajout de texte explicatif

--- src/fonction_adaption.cpp
+++ src/fonction_adaption.cpp
@@ -6,7 +6,12 @@
 using namespace std;
 #include "genetique.cpp"
 
-double DistanceParcours(Individu I,vector<vector<double>> Matrice){
+
+
+
+
+//Une valeur plus faible de la fonction d'adaption signifie une meilleur adaptation de l'individu.
+double DistanceParcours(Individu I,vector<vector<double>> &Matrice){
     double dist = 0.;
     int n = I.itineraire.size();
     for(int i = 0; i < n-1;i++ ){
