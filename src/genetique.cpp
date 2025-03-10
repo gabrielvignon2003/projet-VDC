6f2cef4e7cdae679042a1f77f417cea4c0ae8c5d
Gabriel Vignon <gabriel.vignon@ensta-paris.fr>
2025-03-10 22:17:48 +0100

    amélioration des classes

--- src/genetique.cpp
+++ src/genetique.cpp
@@ -3,31 +3,58 @@
 #include <ctime>
 #include <vector>
 #include <utility>
-#include "fonction_adaption.cpp"
+#include <algorithm>
+
 using namespace std;
 
+
+//Individu = itinéraire
 class Individu{
     public:
-        vector<int> villes;
-        int depart;
-        vector<int> itineraire;
-        double adaptation;
+        vector<int> itineraire; //itinéraire effectif
+        double adaptation; //adaptation = inverse de la distance de l'itinéraire
 
         //constructeur paramétrique
-        Individu(vector<int> villes_, int depart_, vector<int> itineraire_,double adaptation_):
-        villes(villes_),depart(depart_),itineraire(itineraire_),adaptation(adaptation_){};
-        
+        Individu(vector<int> villes_, int depart_, vector<int> itineraire_,double adaptation_=0):
+        itineraire(itineraire_),adaptation(adaptation_){};
+
         //constructeur par copie
-        Individu(const Individu& autre):villes(autre.villes),depart(autre.depart),
+        Individu(const Individu& autre):
         itineraire(autre.itineraire),adaptation(autre.adaptation){};
+
+        //évaluation de l'itinéraire
+        void evaluer(vector<vector<double>> matrice_des_distances){
+            adaptation = 1./distance_parcours(*this, matrice_des_distances);
+        }
+
+        //affichage de l'itinéraire
+        void afficher(){
+            auto it=(*this).itineraire.begin();
+            while(it !=(*this).itineraire.end()){
+                cout << *it << "-->";
+                it++;
+            }
+        }
 };
 
+double distance_parcours(Individu I,vector<vector<double>> matrice_des_distances){
+    double dist = 0.;
+    int n = I.itineraire.size();
+    for(int i = 0; i < n-1;i++ ){
+        dist += matrice_des_distances[I.itineraire[i]][I.itineraire[i+1]];
+    }
+    return dist;
+}
+
+
+// Population = ensemble d'itinéraires
 class Population{
     public:
         int taille;
         vector<Individu> composition;
 };
 
+// hybridation = croisement de deux individus
 pair<Individu,Individu> hybridation(Individu& papa, Individu& maman){
     Individu enfant_pm(maman);
     Individu enfant_mp(maman);
@@ -51,7 +78,7 @@ pair<Individu,Individu> hybridation(Individu& papa, Individu& maman){
 }
 
 
-
+// mutation d'un individu
 Individu mutation(Individu parent){
     srand(time(0));
     int k = 1 + rand() % (parent.itineraire.size()-2);
@@ -64,12 +91,14 @@ Individu mutation(Individu parent){
     return enfant;
 }
 
-int distance_population(Population& P){
+
+
+int distance_population(Population& P, vector<vector<double>> matrice_des_distances){
 
     int S = 0;
-    auto it = reproducteurs.composition.begin();
-    while(it!=reproducteurs.composition.end()){
-        S+=distance_parcours(it);
+    auto it = P.composition.begin();
+    while(it!=P.composition.end()){
+        S+=distance_parcours(*it, matrice_des_distances);
         it++;
     }
     return S;
@@ -78,10 +107,10 @@ int distance_population(Population& P){
 
 enum modes_selection_reproducteurs {ROULETTE, RANG, TOURNOI, EUGENISME};
 Population selection_reproducteurs(const Population& adultes, enum modes_selection_reproducteurs mode_choisi = ROULETTE){
+    Population reproducteurs;
     if(mode_choisi == ROULETTE){
         auto iterateur = adultes.composition.begin();
         int S = 0;
-        Population reproducteurs;
         while(iterateur != adultes.composition.end()){
             S = S + (*iterateur).adaptation;
             iterateur++;
@@ -100,37 +129,30 @@ Population selection_reproducteurs(const Population& adultes, enum modes_selecti
             }
             reproducteurs.composition.push_back(*iterateur);
         }
-        return reproducteurs;
     }
     // Les trois autres modes de sélection des reproducteurs pourront être implémentés plus tard
-
+    return reproducteurs;
 }
 
-Individu minorant_population(const Population& population_en_vrac){
-    auto iterateur = population_en_vrac.composition.begin();
-    Individu minorant = *iterateur;
-    while(iterateur != population_en_vrac.composition.end()){
-        if((*iterateur).adaptation<minorant.adaptation){
-            minorant = *iterateur;
-        }
-        iterateur++;
-    }
-    return minorant;()
+vector<Individu>::iterator minorant_population(std::vector<Individu>& composition){
+    return std::min_element(composition.begin(), composition.end(),
+                            [](const Individu& a, const Individu& b) {
+                                return a.adaptation < b.adaptation;
+                            });
 }
 
-
-Population classement_population(const Population& population_en_vrac){
+Population classement_population(const Population& population_en_vrac) {
     Population temp(population_en_vrac);
     Population population_classee;
-    int i=0;
-    while(i<population_en_vrac.composition.size()){
-        population_classee.composition.push_back(minorant_population(temp));
-        temp.composition.erase(minorant_population(temp));
-        i++;
-    }
-    
-}
 
+    while (!temp.composition.empty()) {
+        auto iterateur_minorant = minorant_population(temp.composition);
+        population_classee.composition.push_back(*iterateur_minorant);
+        temp.composition.erase(iterateur_minorant);
+    }
+
+    return population_classee;
+}
 enum modes_selection_pop_finale{ENFANTS_PRIORITAIRES, ELITISME};
 Population selection_population_finale(const Population& parents, const Population& enfants, enum modes_selection_pop_finale mode_choisi = ENFANTS_PRIORITAIRES, int nombre_parents_survivants=0){
     Population nouvelle_generation;
@@ -154,6 +176,7 @@ Population selection_population_finale(const Population& parents, const Populati
     }
 
     // L'eugénisme pourra être implanté plus tard 
+    return nouvelle_generation;
 }
 
 int main(){
